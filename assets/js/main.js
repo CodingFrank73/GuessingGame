2209280a686cbb5392db0c18d64a43d470004999
Frank <codingfrank73@gmail.com>
2022-02-21 19:20:37 +0100

    responsiv

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,9 @@
-
-
 let numberOfRounds = document.getElementById("numberOfRounds");
+let rounds = 4;
+let randomValue = getRandomIntInclusive();
+let anzahlRunde = 0;
 
 var radios = document.querySelectorAll('input[type=radio][name="rounds"]');
-
 radios.forEach(radio => radio.addEventListener('change', () => {
 
     if (radios.item(0).checked == true) {
@@ -35,27 +35,24 @@ function getRandomIntInclusive() {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-let rounds = 4;
-let randomValue = getRandomIntInclusive();
-let anzahlRunde = 0;
-
 function start() {
     if (anzahlRunde == 0) {
         document.getElementById("formRadio").style.display = "none";
-        document.getElementById("formShow").style.display = "flex";
+        document.getElementById("formShow").style.display = "block";
     }
 
     let input = document.getElementById("input").value;
 
+    console.log(randomValue);
     if (rounds > anzahlRunde) {
         let text;
 
         if (input == randomValue) {
-            text = `${anzahlRunde} treffer`;
+            text = `YES!! You got me in ${anzahlRunde + 1} guesses, I'm ${randomValue}. <span><a href="">You win!! Play Again</a></span> `;
         } else if (input > randomValue) {
-            text = `${anzahlRunde + 1} - suche kleiner`;
+            text = `${anzahlRunde + 1} - You need to guess lower than ${input}, try again...`;
         } else {
-            text = `${anzahlRunde + 1} - suche größer`;
+            text = `${anzahlRunde + 1} - You need to guess higher than ${input}, try again...`;
         }
 
         anzahlRunde++;
@@ -65,8 +62,4 @@ function start() {
         document.getElementById("counter").innerHTML = `${anzahlRunde} / ${rounds}`;
         document.getElementById("help").innerHTML += `${text} <br>`;
     }
-
 }
-
-
-
